08082b125bab62de6c486d16efbded1ce41975d6
Anam <anasim@noodle.org>
2013-12-17 16:03:51 -0500

    tip calculator version 2

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,3 +1,4 @@
+'''
 meal = 100.00
 tax = 0.20
 tip = 0.10
@@ -7,24 +8,24 @@ meal_with_tax = meal + tax_value
 tip_value = meal * tip
 total = meal_with_tax + tip_value
 
-print meal
-print tax_value
-print tip_value
-print total
+print "${0:.2f}".format(meal)
+print "${0:.2f}".format(tax_value)
+print "${0:.2f}".format(tip_value)
+print "${0:.2f}".format(total)
 
 '''
-meal = raw_input(meal cost before tax)
-tax = raw_input(tax charged)
-tax = raw_input(tax charged)
-tip = 0.1 * 100
+meal = float(raw_input("meal cost before tax"))
+tax = float(raw_input("tax rate"))
+tip = float(raw_input("tip rate"))
 
-tax_value = ({:.2f}.format(tax))
-meal_with_tax = ({:.2f}.format(meal)) + tax_value
-tip_value = raw_input("how much is the tip?")
+tax_value = tax * meal
+meal_with_tax = meal + tax_value
+tip_value = meal_with_tax * tip
 total = meal_with_tax + tip_value
 
-print meal
-print tax_value
-print tip_value
-print total
-'''
+print "${0:.2f}".format(meal)
+print "${0:.2f}".format(tax_value)
+print "${0:.2f}".format(tip_value)
+print "${0:.2f}".format(total)
+
+print "Tipping at a rate of {0}%, you should leave ${1:.2f} for a tip.".format(int(100*tip), tip_value)
